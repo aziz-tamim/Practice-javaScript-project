381243091ae63afe163046d90f9fd4f4a1800b4e
abdul-aziz <tamimaziz2007@gmail.com>
2024-08-11 09:06:04 +0600

    updat

--- project-01/app.js
+++ project-01/app.js
@@ -25,7 +25,7 @@ function checkEmail(input) {
   if (re.test(input.value.trim())) {
     showSuccess(input);
   } else {
-    showError(input, "Email is not valid");
+    showError(input, "Email is not valid"); 
   }
 }
 
