b5675cf75a0836692367dec8dae811c069b98fe5
abdul-aziz <tamimaziz2007@gmail.com>
2024-08-15 12:26:13 +0600

    update

--- project-02/app.js
+++ project-02/app.js
@@ -1,17 +1,27 @@
 const container = document.querySelector(".container");
-const seat = document.querySelectorAll(".seat");
+const seat = document.querySelectorAll(".row .seat:not(.occupied)");
 const count = document.getElementById("count");
 const total = document.getElementById("total");
 const movie = document.getElementById("movie");
 
-const ticket = movie.value;
+const ticket = +movie.value;
 
-container.addEventListener('click', (event) => {
-    if(event.target.classList.contains('seat') && !event.target.classList.contains('occupied')) {
-        event.target.classList.toggle('selected');
-    }
-})
+container.addEventListener("click", (event) => {
+  if (
+    event.target.classList.contains("seat") &&
+    !event.target.classList.contains("occupied")
+  ) {
+    event.target.classList.toggle("selected");
+  }
+  selectedCount();
+});
 
 function selectedCount() {
-    const selectSeat = document.querySelectorAll(".seat.selected");
+  const selectSeat = document.querySelectorAll(".row .seat.selected");
+
+  const selectSeatCount = selectSeat.length;
+
+  count.innerText = selectSeatCount;
+  total.innerText = selectSeatCount * ticket;
 }
+
