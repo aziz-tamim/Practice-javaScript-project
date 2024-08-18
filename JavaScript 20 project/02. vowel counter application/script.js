88badd38b2768051d85451e302f1ad268afedd31
abdul-aziz <tamimaziz2007@gmail.com>
2024-08-18 12:15:22 +0600

    update

--- JavaScript 20 project/02. vowel counter application/script.js
+++ JavaScript 20 project/02. vowel counter application/script.js
@@ -1,11 +1,13 @@
 function countVowels() {
-    const textArea = document.getElementById('textArea').value;
-    const vowels = ['a', 'e', 'i', 'o', 'u'];
-    let vowelsCount = 0;
-    for(let char of textArea.toLowerCase()) {
-        if (vowels.includes(char)) {
-            vowelsCount++;
-        }
+  const textArea = document.getElementById("textArea").value;
+  const vowels = ["a", "e", "i", "o", "u"];
+  let vowelsCount = 0;
+  for (let char of textArea.toLowerCase()) {
+    if (vowels.includes(char)) {
+      vowelsCount++;
     }
-    document.getElementById('result').textContent = `Total vowels: ${vowelsCount}`;
+  }
+  document.getElementById(
+    "result"
+  ).textContent = `Total vowels: ${vowelsCount}`;
 }
