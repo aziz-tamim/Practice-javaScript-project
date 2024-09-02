8b9d0ddda3952823a38638dd0519223789004da1
abdul-aziz <tamimaziz2007@gmail.com>
2024-09-02 18:03:54 +0600

    update

--- JavaScript-20-project/04-generate-random-password/script.js
+++ JavaScript-20-project/04-generate-random-password/script.js
@@ -1,13 +1,40 @@
 document.getElementById("range").addEventListener("input", rangeChange);
 document.getElementById("copyPassword").addEventListener("click", copyPassword);
-document
-  .getElementById("generatePassword")
-  .addEventListener("click", generatePassword);
+document.getElementById("generatePassword").addEventListener("click", generatePassword);
 
-function updateRangeBackground(value) {}
-
+function updateRangeBackground(value) {
+  const max = document.getElementById("range").max;
+  const percentage = (value / max) * 100;
+  document.getElementById("updateRange").style.background = `linear-gradient(90deg, #4caf50 ${percentage}%, #ddd ${percentage}%)`;
+}
 function rangeChange() {
   const length = this.value;
   document.getElementById("updateLength").textContent = length;
   updateRangeBackground(length);
+}
+
+function generatePassword() {
+    const length = document.getElementById("range").value;
+    const includeLowercase = document.getElementById("lowercase").checked;
+    const includeUppercase = document.getElementById('uppercase').checked;
+    const includeNumbers = document.getElementById('numbers').checked;
+    const includeSymbols = document.getElementById("symbols").checked;
+
+    const lowerChars = "abcdefghijklmnopqrstuvwxyz";
+    const upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const numberChars = "0123456789";
+    const symbolChars = "@#$%^&*()_+~|}{[]:;?><,./-=";
+
+    let availableChars = "";
+    if (includeLowercase) availableChars +=lowerChars;
+    if (includeUppercase) availableChars +=upperChars;
+    if (includeNumbers) availableChars +=numberChars;
+    if (includeSymbols) availableChars +=symbolChars;
+
+    let password = "";
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * availableChars.length)
+        password += availableChars[randomIndex];
+    }
+    document.getElementById("password").value = password
 }
