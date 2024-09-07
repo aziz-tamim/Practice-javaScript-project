b240a41c94470061bf5285c50801fd4b52ee7147
abdul-aziz <tamimaziz2007@gmail.com>
2024-09-07 14:35:04 +0600

    update

---
+++ JavaScript-20-project/05-cash-calculator-application/script.js
@@ -0,0 +1,14 @@
+const notesValues = {
+  note2000: 2000,
+  note500: 500,
+  note200: 200,
+  note100: 100,
+  note50: 50,
+  note20: 20,
+  note10: 10,
+  note5: 5,
+  note2: 2,
+  note1: 1,
+};
+
+function calculateTotal() {}
