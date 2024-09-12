e458d654be65ed7d5eebba4cc8963a8f18b47f27
abdul-aziz <tamimaziz2007@gmail.com>
2024-09-12 18:09:01 +0600

    update

--- JavaScript-20-project/05-cash-calculator-application/some.js
+++ JavaScript-20-project/05-cash-calculator-application/some.js
@@ -1,7 +1,7 @@
 // Function to generate a random hex color
 function getRandomHexColor() {
     return "#" + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0');
-  }
+}
   
   // Function to apply random colors to all blocks on page load
   function applyRandomColors() {
