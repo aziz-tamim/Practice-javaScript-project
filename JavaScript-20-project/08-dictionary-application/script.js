4546ce6984f5c85d0549ddd6d5e06e1123b66683
abdul-aziz <tamimaziz2007@gmail.com>
2024-09-28 15:12:49 +0600

    update

--- JavaScript-20-project/08-dictionary-application/script.js
+++
@@ -1,7 +0,0 @@
-const input = document.getElementById("dictionaryInput");
-const btn = document.getElementById("searchBtn");
-const dictionary = document.querySelector("dictionary__wrapper");
-
-document.addEventListener("click", function() {
-
-})
