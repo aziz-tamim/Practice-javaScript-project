ba2d17f35a71883b9eb6048aeb83cc37ed46bfc4
abdul-aziz <tamimaziz2007@gmail.com>
2024-10-02 18:12:00 +0600

    update

--- JavaScript-20-project/11-generate-random-paragraph/script.js
+++ JavaScript-20-project/11-generate-random-paragraph/script.js
@@ -1,30 +1,35 @@
 function generate() {
-    const numberParagraph = document.getElementById('items').value;
+    const numberOfParagraphs = document.getElementById('items').value;
     const dataDiv = document.getElementById('data');
-    
     dataDiv.innerHTML = '';
     
-    for (let i = 0; i < numberParagraph; i++) {
+    for (let i = 0; i < numberOfParagraphs; i++) {
         const paragraph = document.createElement('p');
         paragraph.innerText = generateRandomParagraph();
-    }
+        dataDiv.appendChild(paragraph);
+    } 
 }
 
 function generateRandomParagraph() {
     const sentences = [
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        "Vestibulm nec velit vitae urna lacinia feugiat.",
-        "Curabitur consequat felis eget sum luctus, eget vehicula ligula secelerisque.",
+        "Vestibulum nec velit vitae urna lacinia feugiat.",
+        "Curabitur consequat felis eget sem luctus, eget vehicula ligula scelerisque.",
         "Nulla facilisi. Suspendisse sit amet purus nec metus elementum accumsan.",
         "Donec suscipit sapien non ipsum commodo, id vestibulum est varius.",
         "Integer aliquet massa id nisi eleifend viverra.",
         "Praesent vel ligula eget mauris dapibus gravida.",
         "Cras efficitur nisi at purus euismod, et tempus urna ultrices.",
-        "Morbi id saoien nec dui gravida pretium  a vel lacus.",
+        "Morbi id sapien nec dui gravida pretium a vel lacus.",
         "Quisque ac felis auctor, fermentum nulla at, tempus velit."
     ];
-    const sentenceCount = Math.floor(Math.random * 5) + 3;
+
+    const sentenceCount = Math.floor(Math.random() * 5) + 3;
     let paragraph = '';
 
+    for (let i = 0; i < sentenceCount; i++) {
+        paragraph += sentences[Math.floor(Math.random() * sentences.length)] + ' ';
+    }
 
+    return paragraph.trim();
 }
