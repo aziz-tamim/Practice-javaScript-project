3b2e0b9fc560de4d8d3be488885d48c6cc745b0c
abdul-aziz <tamimaziz2007@gmail.com>
2024-10-08 14:59:06 +0600

    update

--- JavaScript-20-project/12-loan-calculator-application/script.js
+++ JavaScript-20-project/12-loan-calculator-application/script.js
@@ -1,13 +1,18 @@
-document.getElementById("calculate").addEventListener("click", function() {
-    const loanAmount = parseFloat(document.getElementById("calculatorAmount").value);
-    const interestRate = parseFloat(document.getElementById("calculatorRate").value) / 100/ 12;
-    const loanTerm = parseInt(document.getElementById("calculatorTerm").value);
+document.getElementById("calculate").addEventListener("click", function () {
+  const loanAmount = parseFloat(
+    document.getElementById("calculatorAmount").value
+  );
+  const interestRate =
+    parseFloat(document.getElementById("calculatorRate").value) / 100 / 12;
+  const loanTerm = parseInt(document.getElementById("calculatorTerm").value);
 
-    const monthlyPayment = (loanAmount * interestRate * Math.pow(1 + interestRate, loanTerm)) / (math.pow(1 + interestRate, loanTerm) - 1);
+  const monthlyPayment =
+    (loanAmount * interestRate * Math.pow(1 + interestRate, loanTerm)) /
+    (Math.pow(1 + interestRate, loanTerm) - 1);
 
-    const totalInterest = (monthlyPayment * loanTerm) - loanAmount;
-    const resultElement = document.querySelector(".final-result");
-    resultElement.innerHTML = `Monthly Payment: ${monthlyPayment.toFixed(2)}<br />
+  const totalInterest = monthlyPayment * loanTerm - loanAmount;
+  const resultElement = document.querySelector(".final-result");
+  resultElement.innerHTML = `Monthly Payment: ${monthlyPayment.toFixed(2)}<br />
     </br>
     Total Interest: ${totalInterest.toFixed(2)}
     `;
