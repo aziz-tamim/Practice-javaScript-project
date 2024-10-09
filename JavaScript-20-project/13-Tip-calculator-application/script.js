c2de0ddfa3c4c8d544c627a5ef155859990b7f05
abdul-aziz <tamimaziz2007@gmail.com>
2024-10-09 14:57:36 +0600

    update

---
+++ JavaScript-20-project/13-Tip-calculator-application/script.js
@@ -0,0 +1,6 @@
+document.getElementById("calculateBtn").addEventListener("click", function() {
+  let billAmount = parseFloat(document.getElementById("billAmount").value);
+  let serviceRating = document.getElementById("serviceRating").value;
+  let numberOfPeople = parseFloat(document.getElementById("numberOfPeople").value);
+  let mealType = document.getElementById("mealType").value;
+})
