05000934f62244c4bc17c956a10358e2fd53fcd6
abdul-aziz <tamimaziz2007@gmail.com>
2024-10-09 16:44:11 +0600

    update

--- JavaScript-20-project/13-Tip-calculator-application/script.js
+++ JavaScript-20-project/13-Tip-calculator-application/script.js
@@ -3,4 +3,18 @@ document.getElementById("calculateBtn").addEventListener("click", function() {
   let serviceRating = document.getElementById("serviceRating").value;
   let numberOfPeople = parseFloat(document.getElementById("numberOfPeople").value);
   let mealType = document.getElementById("mealType").value;
+
+  let tipPercentage;
+  switch(serviceRating) {
+    case 'poor':
+        tipPercentage = 0.05;
+        break;
+    case 'average':
+        tipPercentage = 0.10;
+        break;
+    case 'good':
+        tipPercentage = 0.15;
+        break;
+    
+  }
 })
