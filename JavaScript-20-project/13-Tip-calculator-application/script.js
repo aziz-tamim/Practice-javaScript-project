61946b59d66669f0c78628525b72db65ff450891
abdul-aziz <tamimaziz2007@gmail.com>
2024-10-10 07:58:30 +0600

    update

--- JavaScript-20-project/13-Tip-calculator-application/script.js
+++ JavaScript-20-project/13-Tip-calculator-application/script.js
@@ -1,27 +1,36 @@
-document.getElementById("calculateBtn").addEventListener("click", function() {
-  let billAmount = parseFloat(document.getElementById("billAmount").value);
-  let serviceRating = document.getElementById("serviceRating").value;
-  let numberOfPeople = parseFloat(document.getElementById("numberOfPeople").value);
-  let mealType = document.getElementById("mealType").value;
+document.getElementById('calculateBtn').addEventListener('click', function() {
+    let billAmount = parseFloat(document.getElementById('billAmount').value);
+    let serviceRating = document.getElementById('serviceRating').value;
+    let numberOfPeople = parseInt(document.getElementById('numberOfPeople').value);
+    let mealType = document.getElementById('mealType').value;
 
-  let tipPercentage;
-  switch(serviceRating) {
-    case 'poor':
-        tipPercentage = 0.05;
-        break;
-    case 'average':
-        tipPercentage = 0.10;
-        break;
-    case 'good':
-        tipPercentage = 0.15;
-        break;
-    case 'excellent':
-        tipPercentage = 0.20;
-        break;
-    default:
-        tipPercentage = 0.10;
-  }
+    let tipPercentage;
+    switch(serviceRating) {
+        case 'poor':
+            tipPercentage = 0.05;
+            break;
+        case 'average':
+            tipPercentage = 0.10;
+            break;
+        case 'good':
+            tipPercentage = 0.15;
+            break;
+        case 'excellent':
+            tipPercentage = 0.20;
+            break;
+        default:
+            tipPercentage = 0.10;
+    }
 
-  let tipAmount = billAmount * tipPercentage;
-  let totalPerPerson = (billAmount + tipAmount) / billAmount;
-})
+
+    let tipAmount = billAmount * tipPercentage;
+
+    let totalPerPerson = (billAmount + tipAmount) / numberOfPeople;
+
+    document.getElementById('result').innerHTML = `
+        <h3 class="result">Result:</h3>
+        <p>Meal Type: ${mealType.charAt(0).toUpperCase() + mealType.slice(1)}</p>
+        <p>Tip Amount: $${tipAmount.toFixed(2)}</p>
+        <p>Total Per Person: $${totalPerPerson.toFixed(2)}</p>
+    `;
+});
