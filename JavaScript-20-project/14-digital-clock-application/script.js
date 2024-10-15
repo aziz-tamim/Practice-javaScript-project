97a856b1efd449818f67db90191b7f0ebda751c5
abdul-aziz <tamimaziz2007@gmail.com>
2024-10-15 08:24:27 +0600

    update

--- JavaScript-20-project/14-digital-clock-application/script.js
+++ JavaScript-20-project/14-digital-clock-application/script.js
@@ -8,12 +8,21 @@ function showTime() {
     if (hours == 0) {
         hours = 12;
     }
+
     if (hours > 12) {
         hours = hours - 12;
         session = "PM";
     }
+
     hours = (hours < 10) ? "0" + hours : hours;
     minutes = (minutes < 10) ? "0" + minutes : minutes;
+    seconds = (seconds < 10) ? "." + seconds : seconds;
+
+    let time = hours + ":" + minutes + ":" + seconds + " " + session;
+    document.getElementById("MyClockDisplay").innerText = time;
+    document.getElementById("MyClockDisplay").textContent = time;
+
+    setTimeout(showTime, 1000);
 }
 
 showTime();
