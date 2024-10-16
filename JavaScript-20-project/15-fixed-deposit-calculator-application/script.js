bc2547dc94a0a1758f51a616957207d219e8add1
abdul-aziz <tamimaziz2007@gmail.com>
2024-10-16 15:21:59 +0600

    update

--- JavaScript-20-project/15-fixed-deposit-calculator-application/script.js
+++ JavaScript-20-project/15-fixed-deposit-calculator-application/script.js
@@ -1,13 +1,17 @@
-document.getElementById("calculateBtn").addEventListener("click", function() {
-    const principal = parseFloat(document.getElementById("principal").value);
-    const rateOfInterest = parseFloat(document.getElementById("interestRate").value);
-    const tenure = parseFloat(document.getElementById("tenure").value);
+document.getElementById("calculateBtn").addEventListener("click", function () {
+  const principal = parseFloat(document.getElementById("principal").value);
+  const rateOfInterest = parseFloat(
+    document.getElementById("interestRate").value
+  );
+  const tenure = parseFloat(document.getElementById("tenure").value);
 
-    if(isNaN(principal) || isNaN(rateOfInterest) || isNaN(tenure)) {
-        alert("Please enter valid input in all fields");
-        return;
-    }
+  if (isNaN(principal) || isNaN(rateOfInterest) || isNaN(tenure)) {
+    alert("Please enter valid input in all fields");
+    return;
+  }
 
-    const maturityAmount = principal * Math.pow((1 + (rateOfInterest / 100)), tenure);
-    document.getElementById('result').textContent = `Maturity Amount: ${maturityAmount.toFixed(2)}`;
+  const maturityAmount = principal * Math.pow(1 + rateOfInterest / 100, tenure);
+  document.getElementById(
+    "result"
+  ).textContent = `Maturity Amount: ${maturityAmount.toFixed(2)}`;
 });
