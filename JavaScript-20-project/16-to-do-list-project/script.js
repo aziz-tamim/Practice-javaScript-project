1a3bc0f448a6931e91c9f9efe55c022d13d5d11f
abdul-aziz <tamimaziz2007@gmail.com>
2024-10-18 08:18:06 +0600

    update

---
+++ JavaScript-20-project/16-to-do-list-project/script.js
@@ -0,0 +1,43 @@
+document.getElementById('add-button').addEventListener("click", function() {
+    let inputBox = document.getElementById('input-box');
+    let task = inputBox.value.trim();
+
+    if (task !== "") {
+        addTaskToList(task);
+        saveTaskToLocalStorage(task);
+        inputBox.value = task;
+    } else {
+        alert("please enter a task!")
+    }
+});
+
+function loadTasks() {
+    let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    tasks.forEach(task => {
+        addTaskToList(task);
+    });
+}
+
+function addTaskToList(task) {
+    let listContainer = document.getElementById("list-container");
+
+    let newTask = document.createElement("li");
+    newTask.textContent = task;
+ 
+    let crossIcon = document.createElement("span");
+    crossIcon.textContent = "✖";
+    crossIcon.className = "cross-icon";
+
+    newTask.appendChild(crossIcon);
+    listContainer.appendChild(newTask);
+
+    crossIcon.addEventListener("click", function() {
+        listContainer.removeChild(newTask);
+        removeTaskFromLocalStorage(task)
+        console.log(removeTaskFromLocalStorage)
+    })
+
+    function saveTaskToLocalStorage(task) {
+        
+    }
+}
