00c885a7b6dbcd411417d592e3b03a5a28f89f1c
abdul-aziz <tamimaziz2007@gmail.com>
2024-10-19 16:34:47 +0600

    update

--- JavaScript-20-project/17-BMI-calculator-application/script.js
+++ JavaScript-20-project/17-BMI-calculator-application/script.js
@@ -12,4 +12,21 @@ document.getElementById('bmiBtn').addEventListenter("click", function() {
     inches = parseInt(inches);
     weight = parseFloat(weight);
 
+    let heightInInches = (feet * 12) + inches;
+    let heightInMeters = heightInInches * 0.0254;
+
+    let bmi = weight / (heightInMeters * heightInInches);
+
+    let category = '';
+    if (bmi < 16) {
+        category = "You are savere Thin";
+    } else if (bmi >= 16 && bmi <= 17) {
+        category = "You are Moderate Thin";
+    } else if (bmi > 17 && bmi <= 18.5) {
+        category = "You are mid Thin";
+    } else if (bmi > 18 && bmi <=25) {
+        category = "You are Normal";
+    } else {
+        category = "category"
+    }
 });
