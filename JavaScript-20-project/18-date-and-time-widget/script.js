b26c9c1e3806095ad090c8e8e1ddcd14efca106a
abdul-aziz <tamimaziz2007@gmail.com>
2024-10-22 16:33:22 +0600

    update

--- JavaScript-20-project/18-date-and-time-widget/script.js
+++ JavaScript-20-project/18-date-and-time-widget/script.js
@@ -1,52 +1,52 @@
-let dateContainer = document.querySelector('.date-container');
-let hoursContainer = document.querySelector('.hours');
-let minutesContainer = document.querySelector('.minutes');
-let secondsContainer = document.querySelector('.seconds');
+let dateContainer = document.querySelector(".date-container");
+let hoursContainer = document.querySelector(".hours");
+let minutesContainer = document.querySelector(".minutes");
+let secondsContainer = document.querySelector(".seconds");
 
 const weekdays = [
-    "Sunday",
-    "Monday",
-    "Thursday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday"
-]
+  "Sunday",
+  "Monday",
+  "Thursday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 
 const monthNames = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
-]
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 
 function formatTime(time) {
-    return time < 10 ? '0' + time : time;
+  return time < 10 ? "0" + time : time;
 }
 
 function updateClock() {
-    const today = new Date();
-    let date = today.getDate();
-    let day = weekdays[today.getDay()];
-    let month = monthNames[today.getMonth()];
-    
-    let hours = formatTime(today.getHours());
-    let minutes = formatTime(today.getMinutes());
-    let seconds = formatTime(today.getSeconds());
+  const today = new Date();
+  let date = today.getDate();
+  let day = weekdays[today.getDay()];
+  let month = monthNames[today.getMonth()];
 
-    dateContainer.innerHTML = `<p>${day}</p><p><span>${date}</span></p><p>${month}</p>`;
+  let hours = formatTime(today.getHours());
+  let minutes = formatTime(today.getMinutes());
+  let seconds = formatTime(today.getSeconds());
 
-    hoursContainer.textContent = hours + ":";
-    minutesContainer.textContent = minutes + ":";
-    secondsContainer.textContent = seconds;
+  dateContainer.innerHTML = `<p>${day}</p><p><span>${date}</span></p><p>${month}</p>`;
+
+  hoursContainer.textContent = hours + ":";
+  minutesContainer.textContent = minutes + ":";
+  secondsContainer.textContent = seconds;
 }
 
 setInterval(updateClock, 1000);
