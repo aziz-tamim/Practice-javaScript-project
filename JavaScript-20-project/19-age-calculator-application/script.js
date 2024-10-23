a2fbf0385f8fe88d5c142c8c2ca54e73520b254d
abdul-aziz <tamimaziz2007@gmail.com>
2024-10-23 17:48:16 +0600

    update

--- JavaScript-20-project/19-age-calculator-application/script.js
+++ JavaScript-20-project/19-age-calculator-application/script.js
@@ -1,27 +1,30 @@
 function calculateAge() {
-    let birthDate = document.getElementById('date').value;
+  let birthDate = document.getElementById("date").value;
 
-    if(!birthDate) {
-        document.getElementById('result').innerHTML = 'Please select your date of birth.'
-        return;
-    }
+  if (!birthDate) {
+    document.getElementById("result").innerHTML =
+      "Please select your date of birth.";
+    return;
+  }
 
-    let birth = new Date(birthDate);
-    let today = new Date();
+  let birth = new Date(birthDate);
+  let today = new Date();
 
-    let ageYears = today.getFullYear() - birth.getFullYear();
-    let ageMonths = today.getMonth() - birth.getMonth();
-    let ageDays = today.getDate() - birth.getDate();
+  let ageYears = today.getFullYear() - birth.getFullYear();
+  let ageMonths = today.getMonth() - birth.getMonth();
+  let ageDays = today.getDate() - birth.getDate();
 
-    if (ageDays < 0) {
-        ageMonths--;
-        ageDays += daysInMonth(today.getMonth() - 1, today.getFullYear());
-    }
+  if (ageDays < 0) {
+    ageMonths--;
+    ageDays += daysInMonth(today.getMonth() - 1, today.getFullYear());
+  }
 
-    if (ageMonths < 0) {
-        ageYears--;
-        ageMonths += 12;
-    }
+  if (ageMonths < 0) {
+    ageYears--;
+    ageMonths += 12;
+  }
 
-    document.getElementById('result').innerHTML = `<p>Your age is <strong>${ageYears}</strong> years, <strong>${ageMonths}</strong> months, and <strong>${ageDays}</strong> days.</p>`;
+  document.getElementById(
+    "result"
+  ).innerHTML = `<p>Your age is <strong>${ageYears}</strong> years, <strong>${ageMonths}</strong> months, and <strong>${ageDays}</strong> days.</p>`;
 }
